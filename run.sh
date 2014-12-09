d9fd5f88896a77c0293525a463814423f69b2776
Pawan Kumar <pawan.kumar@gmail.com>
2014-12-09 19:38:20 +0530

    Starting server and client using mac osx postgres client package

--- run.sh
+++ run.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-export PGPASSWORD=docker
+
+echo -n "USAGE: ./run.sh PASSWORD 1 (for server)/0 (for client) DBNAME"
 
 #Execute below in a separate terminal
 #boot2docker ssh -L 25432:localhost:5432
 
-#docker run --name "postgis" -p 25432:5432 -d -t pawank/postgis
+HOST=`echo $DOCKER_HOST | cut -d: -f2 | cut -c3-`
 
-#Only for mac osx postgres client
-/Library/PostgreSQL/9.3/bin/psql -h $(boot2docker ip) -U docker -p 25432 gis -l
+if [ $# -eq 0 ]
+  then
+        echo -n "USAGE: ./run.sh PASSWORD 1 (for server)/0 (for client) DBNAME"
+        exit
+fi
+export PGPASSWORD=$1
+
+SERVER=$2
+DBNAME=$3
+
+if [[ ${SERVER} == "1" ]]; then
+    docker run --name "postgis" -p 25432:5432 -d -t pawank/postgis
+fi
+if [[ ${SERVER} == "0" ]]; then
+  #Only for mac osx postgres client
+  /Library/PostgreSQL/9.3/bin/psql -h $HOST -U docker -p 25432 $DBNAME -l
+fi
+
+
