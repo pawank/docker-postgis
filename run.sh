058cb04d1305e1ee63c4c9e721fb654dc4b91985
Pawan Kumar <pawan.kumar@gmail.com>
2014-12-09 19:27:17 +0530

    Enable connection from all hosts

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ export PGPASSWORD=docker
 #Execute below in a separate terminal
 #boot2docker ssh -L 25432:localhost:5432
 
+#docker run --name "postgis" -p 25432:5432 -d -t pawank/postgis
+
 #Only for mac osx postgres client
 /Library/PostgreSQL/9.3/bin/psql -h $(boot2docker ip) -U docker -p 25432 gis -l
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 CONF="/etc/postgresql/9.3/main/postgresql.conf"
 
 # Restrict subnet to docker private network
-echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
 echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
